66b138395604892521bb84f1bcb287e78b40c06b
Zakariya Hussian <zakariyahussain1989@gmail.com>
2025-10-05 02:24:07 +0600

    Some extra code remove

--- src/Components/Header/Banner.jsx
+++ src/Components/Header/Banner.jsx
@@ -1,40 +1,15 @@
-// import React from 'react';
-// import { Link } from 'react-router';
-
-// const Banner = () => {
-//     return (
-//         <div
-//             className="hero min-h-screen"
-//             style={{
-//                 backgroundImage:
-//                     "url(https://i.ibb.co/3XTxw1t/bg-1.png)",
-//             }}
-//         >
-//             <div className="hero-overlay"></div>
-//             <div className="hero-content text-neutral-content text-center">
-//                 <div className="">
-//                     <h1 className="mb-5 text-5xl font-bold shadow-2xl">Your Next Car Awaits You.</h1>
-//                     <Link to='availableCars'><button className="btn bg-green-400 text-white">View Available Cars</button></Link>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Banner;
 
 import React from 'react';
 import { Link } from 'react-router'; // ✅ Use react-router-dom
-//import bg1 from '../../assets/img/bg-3.png';
+
 
 const Banner = () => {
     return (
         <div
             className="hero min-h-screen bg-cover bg-center relative"
             style={{
-                //backgroundImage: "url(https://i.ibb.co.com/tpxGyXnW/bg-2.png)",
                 backgroundImage: "url(https://i.ibb.co.com/RpVH7sJ3/bg.png)",
-                //backgroundImage: `url(${bg1})`,
+                
             }}
         >
             {/* Overlay for better contrast */}
